271650e20e82d52b05bc9e8b3e707c0a73d1fb58
Adam Holt <me@adamholt.co.uk>
2012-10-17 15:17:05 +0100

    return some 500s

--- unavailable.rb
+++ unavailable.rb
@@ -1,6 +1,6 @@
 class Unavailable
   def call(env)
     sleep(10)
-    return [504, {"Content-Type" => "text/plain"}, ["Unavailable"]]
+    return [500, {"Content-Type" => "text/plain"}, ["Unavailable"]]
   end
 end
