fcfda4dd5e62e8feb250a03f501fc2c56e0c7db4
Adam Holt <me@adamholt.co.uk>
2012-10-17 16:32:51 +0100

    back to timeouts

--- unavailable.rb
+++ unavailable.rb
@@ -4,7 +4,7 @@ class Unavailable
     if env["PATH_INFO"] == "/v3/badgeville-current.js"
       return [304, {}, []]
     end
-    #sleep(10)
-    return [500, {"Content-Type" => "text/plain"}, ["Unavailable"]]
+    sleep(10)
+    return [504, {"Content-Type" => "text/plain"}, ["Unavailable"]]
   end
 end
