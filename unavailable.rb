bd7de1ad6e4f506e7e98193e239dadf19f414ddf
Adam Holt <me@adamholt.co.uk>
2012-10-17 15:55:46 +0100

    return a 304 for the badgeville.js file

--- unavailable.rb
+++ unavailable.rb
@@ -1,5 +1,9 @@
 class Unavailable
   def call(env)
+    puts env.inspect
+    if env["PATH_INFO"] == "/v3/badgeville-current.js"
+      return [304, {}, []]
+    end
     #sleep(10)
     return [500, {"Content-Type" => "text/plain"}, ["Unavailable"]]
   end
